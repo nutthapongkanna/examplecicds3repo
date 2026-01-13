23b53da47e8751efa3fed83ff48d126edae2e8a5
unknown <nutthapong.ka@bdi.or.th>
2026-01-13 10:40:34 +0700

    approve config

--- dags/example_hello8.py
+++ dags/example_hello8.py
@@ -7,7 +7,7 @@ def hello():
 
 with DAG(
 
-    dag_id="example_hello19",
+    dag_id="example_hello15",
 
 
     start_date=datetime(2024, 1, 1),
@@ -17,6 +17,6 @@ with DAG(
 ) as dag:
     PythonOperator(
 
-        task_id="hello_task8",
+        task_id="hello_task15",
         python_callable=hello,
     )
