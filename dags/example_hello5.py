54a38dd0ac6609d1781cb61cc47f838859b4e5be
unknown <nutthapong.ka@bdi.or.th>
2026-01-13 10:30:12 +0700

    approve config

---
+++ dags/example_hello5.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("hello from DAG on MWAA/S3!")
+
+with DAG(
+    dag_id="example_hello6",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["demo"],
+) as dag:
+    PythonOperator(
+        task_id="hello_task6",
+        python_callable=hello,
+    )
---
+++ dags/example_hello8.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("hello from DAG on MWAA/S3!")
+
+with DAG(
+    dag_id="example_hello9",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["demo"],
+) as dag:
+    PythonOperator(
+        task_id="hello_task9",
+        python_callable=hello,
+    )
