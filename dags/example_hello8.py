12dfaab860d56569deffc196d7bf1a893842b395
unknown <nutthapong.ka@bdi.or.th>
2026-01-13 10:32:21 +0700

    approve config

--- dags/example_hello8.py
+++ dags/example_hello8.py
@@ -6,13 +6,13 @@ def hello():
     print("hello from DAG on MWAA/S3!")
 
 with DAG(
-    dag_id="example_hello9",
+    dag_id="example_hello19",
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
     tags=["demo"],
 ) as dag:
     PythonOperator(
-        task_id="hello_task9",
+        task_id="hello_task19",
         python_callable=hello,
     )
