60b8fd9cb2fa369961084790a699daa6ec4e4678
AzureAD\PassakornVanchana <passakorn.va@bdi.or.th>
2026-01-16 15:32:48 +0700

    add dag

---
+++ dags/example_hello10.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("hello from DAG on MWAA/S3!")
+
+with DAG(
+
+    dag_id="example_hello10",
+
+
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["demo"],
+) as dag:
+    PythonOperator(
+
+        task_id="hello_task10",
+        python_callable=hello,
+    )
